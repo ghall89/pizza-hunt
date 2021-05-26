ed0d49b189ca7bce52443ccea880e70f1c744049
Graham Hall <ghall89@me.com>
2021-05-26 11:42:28 -0400

    add pizza

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -51,6 +51,23 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+  
+  fetch('/api/pizzas', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(respons => response.json())
+  .then(postResponse => {
+    alert('Pizza created sucessfully')
+    console.log(postResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
